e6c8db1d9906f9f56ef62a8623c1b78623eccf6f
Keerti Chaudhary <17keerti@gmail.com>
2022-05-11 12:34:06 -0700

    add seeds file to seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,6 +3,9 @@ const sequelize = require("../config/connection");
 const userSeeds = require("./userSeeds");
 const postSeeds = require("./postSeeds");
 const commentSeeds = require("./commentSeeds");
+const activitySeeds = require("./activitySeeds");
+const hotelSeeds = require("./hotelSeeds");
+const restaurantSeeds = require("./restaurantSeeds");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -10,6 +13,9 @@ const seedDatabase = async () => {
   await userSeeds();
   await postSeeds();
   await commentSeeds();
+  await activitySeeds();
+  await hotelSeeds();
+  await restaurantSeeds();
 
   process.exit(0);
 };
