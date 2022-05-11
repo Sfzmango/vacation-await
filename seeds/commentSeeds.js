6a34e35e57e788adaf23ef2fc0b0e41ee0853fc5
callanhunter <callan57hunter@gmail.com>
2022-05-11 10:47:54 -0700

    add content to postSeeds

--- seeds/commentSeeds.js
+++ seeds/commentSeeds.js
@@ -3,15 +3,15 @@ const { Comment } = require('../models');
 const commentsData = [
     {
         commentText: "Here is a comment on the post",
-        userId: 1,
+        user_id: 1,
     },
     {
         commentText: "This is a great post!",
-        userId: 2,
+        user_id: 2,
     },
     {
         commentText: "Great Travel plans!",
-        userId: 3,
+        user_id: 3,
     },
 ];
 
