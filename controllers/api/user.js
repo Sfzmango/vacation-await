c60f5471abf8336d60fda45b6b557047c0a466b6
Maung Htike <maunghtike1999@gmail.com>
2022-05-11 15:19:25 -0700

    add create user api route

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -1,16 +1,35 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
-router.post("/", async () => {
+// create a new user
+router.post("/", async (req, res) => {
+    try {
+        const dbUserData = await User.create({
+            username: req.body.username,
+            password: req.body.password
+        });
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            res.status(200).json(dbUserData);
+        });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// logs user in
+router.post("/login", async (req, res) => {
 
 });
 
-router.post("/login", async () => {
-
-});
-
-router.post("/logout", async () => {
+// logs user out
+router.post("/logout", async (req, res) => {
 
 });
 
 module.exports = router;
+
+
