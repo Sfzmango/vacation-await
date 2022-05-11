d16f7dcf7ee6c70b6b859d3b895832d750c17c8c
Maung Htike <maunghtike1999@gmail.com>
2022-05-11 14:21:49 -0700

    change single quote to double

--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,7 @@
 // middleware that checks if the user is logged in and if not, redirct them to the login page.
 const auth = (req, res, next) => {
     if (!req.session.logged_in) {
-        res.redirect('/login');
+        res.redirect("/login");
     } else {
         next();
     }
