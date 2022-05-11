d94dfc8a658351357fa9022300b525745bd9a499
Maung Htike <maunghtike1999@gmail.com>
2022-05-10 20:35:48 -0700

    add login function

---
+++ public/js/login.js
@@ -0,0 +1,47 @@
+// checks login
+const loginForm = async (event) => {
+    event.preventDefault();
+
+    let email = document.querySelector('#loginUsername').value.trim();
+    let password = document.querySelector('#loginPassword').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to log in.');
+        }
+    }
+};
+
+// creates an account
+const signupForm = async (event) => {
+    event.preventDefault();
+
+    let email = document.querySelector('#loginUsername').value.trim();
+    let password = document.querySelector('#loginPassword').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to sign up.');
+        }
+    }
+};
+
+// listens to clicks on the login and signup buttons
+document.querySelector('.loginBtn').addEventListener('submit', loginForm);
+document.querySelector('.signupBtn').addEventListener('submit', signupForm);
