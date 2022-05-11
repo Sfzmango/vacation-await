047a1e6627e4fbf96a7f7ef28baf92e64dd2d3b1
Keerti Chaudhary <17keerti@gmail.com>
2022-05-11 12:25:57 -0700

    add hotelseeds to hotel models

--- seeds/hotelSeeds.js
+++ seeds/hotelSeeds.js
@@ -1,1 +1,18 @@
+const { Hotel } = require("../models");
 
+const hotel = [
+  {
+    name: "hotel",
+    price: 250,
+    rating: 4,
+    address: "San francisco",
+    reviews: "Try this hotel!",
+    contact_number: 123456,
+    image_url: "This should be image url",
+    web_url: "This should be web url",
+  },
+];
+
+const hotelSeeds = () => Hotel.bulkCreate(hotel);
+
+module.exports = hotelSeeds;
