f465e82baad14d434b3c167d759e391928a8c31d
Maung Htike <maunghtike1999@gmail.com>
2022-05-11 16:10:47 -0700

    comment out unfinished routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 
 const userRoute = require("./user");
-const plansRoute = require("./plans");
-const commentsRoute = require("./comments");
+// const plansRoute = require("./plans");
+// const commentsRoute = require("./comments");
 
 router.use("/user", userRoute);
-router.use("/plans", plansRoute);
-router.use("/comments", commentsRoute);
+// router.use("/plans", plansRoute);
+// router.use("/comments", commentsRoute);
 
 module.exports = router;
