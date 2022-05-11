cafe3a8d3599a57099e9a30ef3297aa36adb9c3d
Maung Htike <maunghtike1999@gmail.com>
2022-05-11 15:20:21 -0700

    add user logout api route

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -54,7 +54,13 @@ router.post("/login", async (req, res) => {
 
 // logs user out
 router.post("/logout", async (req, res) => {
-
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
 });
 
 module.exports = router;
