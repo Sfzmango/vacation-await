fdac01c45fcfff71122e3cc71744a92994592cc1
callanhunter <callan57hunter@gmail.com>
2022-05-11 11:50:05 -0700

    add structure to main.handlebar

--- seeds/commentSeeds.js
+++ seeds/commentSeeds.js
@@ -2,15 +2,15 @@ const { Comment } = require('../models');
 
 const commentsData = [
     {
-        commentText: "Here is a comment on the post",
+        commentText: 'Here is a comment on the post',
         user_id: 1,
     },
     {
-        commentText: "This is a great post!",
+        commentText: 'This is a great post!',
         user_id: 2,
     },
     {
-        commentText: "Great Travel plans!",
+        commentText: 'Great Travel plans!',
         user_id: 3,
     },
 ];
--- seeds/postSeeds.js
+++ seeds/postSeeds.js
@@ -2,18 +2,18 @@ const { Post } = require("../models");
 
 const postData = [
   {
-    title: "Los Angeles, California",
-    content: "Great place to go! Definitely will go again.",
+    title: 'Los Angeles, California',
+    content: 'Great place to go! Definitely will go again.',
     user_id: 1
   },
   {
-    title: "Miami, Florida",
-    content: "Tough to beat the beaches and the food.",
+    title: 'Miami, Florida',
+    content: 'Tough to beat the beaches and the food.',
     user_id: 2
   },
   {
-    title: "Chicago, Illinois",
-    content: "Best trip i have ever gone on.",
+    title: 'Chicago, Illinois',
+    content: 'Best trip i have ever gone on.',
     user_id: 3
   },
 ];
