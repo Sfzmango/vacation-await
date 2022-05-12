572baf6edd33dc45bb49734bb427282a41687b37
Maung Htike <maunghtike1999@gmail.com>
2022-05-11 17:29:01 -0700

    uncomment plan route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 
 const userRoute = require("./user");
-// const plansRoute = require("./plans");
+const plansRoute = require("./plans");
 // const commentsRoute = require("./comments");
 
 router.use("/user", userRoute);
-// router.use("/plans", plansRoute);
+router.use("/plans", plansRoute);
 // router.use("/comments", commentsRoute);
 
 module.exports = router;
