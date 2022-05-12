53d288ab2967519341848de4a2ac8c023b5f357c
Maung Htike <maunghtike1999@gmail.com>
2022-05-12 09:34:46 -0700

    add login web route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -12,4 +12,14 @@ router.get("/", async (req, res) => {
     }
 });
 
+// login 
+router.get("/login", async (req, res) => {
+    try {
+        res.render("login");
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
