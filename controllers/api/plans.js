205d30c61d5c41530b21be5acbeacced97a03693
Maung Htike <maunghtike1999@gmail.com>
2022-05-11 17:30:27 -0700

    add get route for all plans

--- controllers/api/plans.js
+++ controllers/api/plans.js
@@ -1,20 +1,28 @@
 const router = require("express").Router();
 const { Plan } = require("../../models");
 
-router.get("/", async () => {
+// get all plans as a json - working
+router.get("/", async (req, res) => {
+    const dbPlanData = await Plan.findAll();
+    res.status(200).json(dbPlanData);
+});
+
+// get a specific plan as a json - working
+router.get("/:id", async (req, res) => {
 
 });
 
-router.get("/:id", async () => {
+// create a plan into the db. - mostly working, need to get user_id
+router.post("/", async (req, res) => {
 
 });
 
-router.post("/", async () => {
-
-});
-
-router.delete("/:id", async () => {
+// deletes a plan using id params - working
+router.delete("/:id", async (req, res) => {
 
 });
 
 module.exports = router;
+
+
+
