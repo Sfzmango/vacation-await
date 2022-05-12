fd7fbbfa553f81fc0531093b018b679023299a6b
Maung Htike <maunghtike1999@gmail.com>
2022-05-11 17:29:33 -0700

    fix typo

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,6 @@
 // middleware that checks if the user is logged in and if not, redirct them to the login page.
 const auth = (req, res, next) => {
-    if (!req.session.logged_in) {
+    if (!req.session.loggedIn) {
         res.redirect("/login");
     } else {
         next();
