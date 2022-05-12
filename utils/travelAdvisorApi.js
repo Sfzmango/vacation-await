d0b5e253c7e420481e2fc4bf561d7d59f6607acf
Maung Htike <maunghtike1999@gmail.com>
2022-05-12 15:11:54 -0700

    complete code to randomly fetch a hotel

--- utils/travelAdvisorApi.js
+++ utils/travelAdvisorApi.js
@@ -1,12 +1,22 @@
 const axios = require("axios");
 
-let location_id;
+let location_name = "San Francisco";
+let numAdults = 1;
+let numRooms = 1;
+let numNights = 2;
+let budget = 500;
+let hotelBudget = budget / 8 * 3;
+let hotelFilters;
 
-const options = {
+const randomizer = (e) => {
+    ;
+};
+
+const location = {
     method: "GET",
     url: "https://travel-advisor.p.rapidapi.com/locations/search",
     params: {
-        query: "San Francisco",
+        query: location_name,
         limit: "30",
         units: "mi",
         currency: "USD",
@@ -19,18 +29,88 @@ const options = {
     }
 };
 
-axios.request(options)
+// gets the location id
+axios.request(location)
     .then((res) => {
         let locationRes = res.data;
         console.log(locationRes.data);
         console.log("Testing below");
 
-        // I want to get the location id from the api
-        location_id = locationRes.data[0].result_object.location_id;
+        // get the location id from the api
+        let location_id = locationRes.data[0].result_object.location_id;
         console.log(location_id);
 
+        const hotelOptions = {
+            method: "GET",
+            url: "https://travel-advisor.p.rapidapi.com/hotels/list",
+            params: {
+                location_id: location_id,
+                adults: numAdults,
+                rooms: numRooms,
+                nights: numNights,
+                pricesmax: hotelBudget,
+                currency: "USD",
+                amenities: hotelFilters,
+                limit: "30",
+                sort: "recommended",
+                lang: "en_US"
+            },
+            headers: {
+                "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+                "X-RapidAPI-Key": "3a79c109bemshab3af2a312fbbd8p173ab8jsn5f9ae7456df6"
+            }
+        };
 
-    })
-    .catch(function (err) {
+        // gets a random hotel id
+        axios.request(hotelOptions)
+            .then((res) => {
+                let hotelData = res.data.data;
+                console.log(hotelData);
+                let randomHotelIndex = Math.floor(Math.random() * (hotelData.length));
+                console.log("Selected index: ", randomHotelIndex);
+                let randomHotelId = hotelData[randomHotelIndex].location_id;
+
+
+                const hotelDetails = {
+                    method: "GET",
+                    url: "https://travel-advisor.p.rapidapi.com/hotels/get-details",
+                    params: {
+                        location_id: randomHotelId,
+                        adults: numAdults,
+                        lang: "en_US",
+                        currency: "USD",
+                        nights: numNights,
+                        rooms: numRooms
+                    },
+                    headers: {
+                        "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+                        "X-RapidAPI-Key": "3a79c109bemshab3af2a312fbbd8p173ab8jsn5f9ae7456df6"
+                    }
+                };
+
+                // get hotel details
+                axios.request(hotelDetails)
+                    .then((res) => {
+
+                        let hotelInfo = res.data.data[0];
+
+                        let randomHotelInfo = {
+                            "name": hotelInfo.name,
+                            "price": hotelInfo.price,
+                            "rating": hotelInfo.rating,
+                            "address": hotelInfo.address,
+                            "reviews": hotelInfo.room_tips,
+                            "contact_number": hotelInfo.phone,
+                            "image_url": hotelInfo.photo.images.large.url,
+                            "web_url": hotelInfo.web_url
+                        };
+
+                        console.log(randomHotelInfo);
+
+                        // ============= write save hotel code into db here =========================
+                    });
+            });
+
+    }).catch(function (err) {
         console.error(err);
     });
