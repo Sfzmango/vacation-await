46bee579d6dd5766bf171cd78342cb0af68416d0
Maung Htike <maunghtike1999@gmail.com>
2022-05-12 09:34:59 -0700

    add signup web route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -22,4 +22,14 @@ router.get("/login", async (req, res) => {
     }
 });
 
+// signup
+router.get("/signup", async (req, res) => {
+    try {
+        res.render("signup");
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
