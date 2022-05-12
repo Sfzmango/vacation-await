bf852ed94d445bab23a091bd79d9dfe23c2eec30
Maung Htike <maunghtike1999@gmail.com>
2022-05-11 17:30:56 -0700

    add get route for plan id

--- controllers/api/plans.js
+++ controllers/api/plans.js
@@ -9,7 +9,18 @@ router.get("/", async (req, res) => {
 
 // get a specific plan as a json - working
 router.get("/:id", async (req, res) => {
+    const dbPlanData = await Plan.findOne({
+        where: {
+            id: req.params.id
+        }
+    });
 
+    if (!dbPlanData) {
+        res.status(400).json({ message: "Plan not found." });
+        return;
+    }
+
+    res.status(200).json(dbPlanData);
 });
 
 // create a plan into the db. - mostly working, need to get user_id
@@ -23,6 +34,12 @@ router.delete("/:id", async (req, res) => {
 });
 
 module.exports = router;
+
+
+
+
+
+
 
 
 
