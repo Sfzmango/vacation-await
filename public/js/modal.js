f9df99c6dc2f8ab944466b786ad31497e1c540dc
Keerti Chaudhary <17keerti@gmail.com>
2022-05-12 18:49:56 -0700

    add modal.js in public folder

---
+++ public/js/modal.js
@@ -0,0 +1,20 @@
+function getUserPreferences() {
+  var userPreference = {};
+  userPreference.shopping = document.getElementById("shopping").checked;
+  userPreference.sights = document.getElementById("sights").checked;
+  userPreference.museum = document.getElementById("museum").checked;
+  userPreference.nature = document.getElementById("nature").checked;
+  userPreference.vegan = document.getElementById("vegan").checked;
+  userPreference.glutenfree = document.getElementById("glutenfree").checked;
+  userPreference.vegetarian = document.getElementById("vegetarian").checked;
+  userPreference.asian = document.getElementById("asian").checked;
+  userPreference.american = document.getElementById("american").checked;
+  userPreference.european = document.getElementById("european").checked;
+  userPreference.freewifi = document.getElementById("freewifi").checked;
+  userPreference.spa = document.getElementById("spa").checked;
+  userPreference.noOfAdults = document.getElementById("noOfAdults").checked;
+  userPreference.noOfNights = document.getElementById("noOfNights").checked;
+  userPreference.noOfRooms = document.getElementById("noOfRooms").checked;
+
+  return userPreference;
+}
